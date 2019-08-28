79c32455a6fd8e7c3a516b53141d48789f0a1bb9
Sebastian Beleño <sebasbeleno15@gmail.com>
2019-08-27 21:58:55 -0500

    task: Prepare to heroku :D

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
-	'url': 'mongodb://localhost:27017/login-node'
+	'url': 'mongodb+srv://admin:GIEM@giem-4mkhr.mongodb.net/login-node?retryWrites=true&w=majority'
 	
 	/* 
-	'url': 'mongodb+srv://admin:GIEM@giem-4mkhr.mongodb.net/login-node?retryWrites=true&w=majority'
 
 	//Atlas
+	'url': 'mongodb://localhost:27017/login-node'
 
 	
 	*/
